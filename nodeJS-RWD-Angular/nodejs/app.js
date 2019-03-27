acdfc0b3b3923fdde9088d39ad2dc5434dbc06a2
Haopeng Song <haopengs@usc.edu>
2019-03-27 03:11:48 -0700

    21%

--- nodeJS-RWD-Angular/nodejs/app.js
+++ nodeJS-RWD-Angular/nodejs/app.js
@@ -1,6 +1,35 @@
-var _ = require('underscore');
+const express = require('express');
+const Joi = require('joi');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const app = express();
 
-var res = _.contains([1,2,3], 2);
 
-console.log(res);
+app.get('/api/zipauto/:prefix' , (req, res) => {
+    //schema for joi
+    const schema = {
+        //not important
+    };
+    //call geonames.org
+    let zipCodeStart = req.params.prefix;
+    let URL = `http://api.geonames.org/`;
+    URL += `postalCodeSearchJSON?postalcode_startsWith=${zipCodeStart}`;
+    URL += `&username=jasonsonghp&country=US&maxRows=5`;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", URL, false);
+    try {
+        xhr.send();
+    } catch (error) {
+        res.send('ERROR');
+        return;
+    }
+    res.send(xhr.responseText);
+});
 
+app.get('/api/courses', (req ,res) => {
+    res.send([1,2,3,4]);
+});
+
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port : ${port}`));
