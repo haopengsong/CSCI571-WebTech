f3f588c8e8d85f86b1f400b751d0302e03f77573
Haopeng Song <haopengs@usc.edu>
2019-03-31 23:22:05 -0700

    55%

--- nodeJS-RWD-Angular/nodejs/app.js
+++ nodeJS-RWD-Angular/nodejs/app.js
@@ -1,21 +1,126 @@
 const express = require('express');
-const Joi = require('joi');
+const cors = require('cors');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const app = express();
 
+let serverFindingService = 'http://svcs.ebay.com/services/search/FindingService/v1';
+serverFindingService += '?OPERATION-NAME=findItemsAdvanced';
+serverFindingService += '&SERVICE-VERSION=1.0.0';
+serverFindingService += '&SECURITY-APPNAME=jasonson-571hw-PRD-016e08212-81d2fca4';
+serverFindingService += '&RESPONSE-DATA-FORMAT=JSON';
+serverFindingService += '&REST-PAYLOAD=true';
+serverFindingService += '&paginationInput.entriesPerPage=50';
 
-app.get('/api/zipauto/:prefix' , (req, res) => {
-    //schema for joi
-    const schema = {
-        //not important
-    };
+function ItemFilter(name) {
+    this.name = name;
+    this.option = [];
+}
+
+app.use(cors());
+
+
+app.get('/api/zipauto/:prefix', (req, res) => {
     //call geonames.org
     let zipCodeStart = req.params.prefix;
-    let URL = `http://api.geonames.org/`;
-    URL += `postalCodeSearchJSON?postalcode_startsWith=${zipCodeStart}`;
-    URL += `&username=jasonsonghp&country=US&maxRows=5`;
+    if (zipCodeStart.length >= 3 && zipCodeStart.length <= 5) {
+        let URL = `http://api.geonames.org/`;
+        URL += `postalCodeSearchJSON?postalcode_startsWith=${zipCodeStart}`;
+        URL += `&username=jasonsonghp&country=US&maxRows=5`;
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", URL, false);
+        try {
+            xhr.send();
+        } catch (error) {
+            res.send('ERROR');
+            return;
+        }
+        res.send(xhr.responseText);
+    }
+});
+
+app.get('/api/finding?', (req, res) => {
+    let apiInquery = "";
+    apiInquery += '&keywords=' + encodeURI(req.query.keywords);
+
+    if (req.query.categoryId !== 'None') {
+        apiInquery += '&categoryId=' + req.query.categoryId;
+    }
+    apiInquery += '&buyerPostalCode=' + req.query.buyerPostalCode;
+
+    //*******userInputFilter
+    let filterData = [];
+
+    //condition
+    if (req.query.condition.new !== 'NoCondition') {
+        let condition_filter = new ItemFilter('Condition');
+        if (req.query.condition.new === 'true') {
+            condition_filter.option.push('New');
+        }
+        if (req.query.condition.used === 'true') {
+            condition_filter.option.push('Used');
+        }
+        if (req.query.condition.unspecified === 'true') {
+            condition_filter.option.push('Unspecified');
+        }
+        filterData.push(condition_filter);
+    }
+
+
+    //shipping
+    if (req.query.shipping.freeshipping !== 'NoShippingOption') {
+        if (req.query.shipping.freeshipping === 'true') {
+            let shipping_filter_free = new ItemFilter('shipping');
+            shipping_filter_free.option.push('FreeShippingOnly');
+            filterData.push(shipping_filter_free);
+        }
+        if (req.query.shipping.localpickup === 'true') {
+            let shipping_filter_local = new ItemFilter('shipping');
+            shipping_filter_local.option.push('LocalPickupOnly');
+            filterData.push(shipping_filter_local);
+        }
+    }
+
+
+    //distance
+    let distance_filter = new ItemFilter('distance');
+    if (req.query.distance !== "") {
+        distance_filter.option.push(req.query.distance);
+    } else {
+        distance_filter.option.push("10");
+    }
+    filterData.push(distance_filter);
+
+    //hidedup
+    let hide_filter = new ItemFilter('HideDuplicateItems');
+    hide_filter.option.push('true');
+
+    filterData.push(hide_filter);
+    //console.log(filterData);
+    //build filterURL
+    let filterURL = "";
+    for (let i = 0; i < filterData.length; i++) {
+        if (filterData[i].name === 'Condition') {
+            filterURL += '&itemFilter(' + i + ').name=Condition';
+            for (let j = 0; j < filterData[i].option.length; j++) {
+                filterURL += '&itemFilter(' + i + ').value(' + j + ')=' + filterData[i].option[j];
+            }
+        } else if (filterData[i].name === 'shipping') {
+            filterURL += '&itemFilter(' + i + ').name=' + filterData[i].option[0];
+            filterURL += '&itemFilter(' + i + ').value=true';
+        } else if (filterData[i].name === 'distance') {
+            filterURL += '&itemFilter(' + i + ').name=MaxDistance';
+            filterURL += '&itemFilter(' + i + ').value=' + filterData[i].option[0];
+        } else if (filterData[i].name === 'HideDuplicateItems') {
+            filterURL += '&itemFilter(' + i + ').name=HideDuplicateItems';
+            filterURL += '&itemFilter(' + i + ').value=true';
+        }
+    }
+    filterURL += '&outputSelector(0)=SellerInfo&outputSelector(1)=StoreInfo';
+    let requestURL = "";
+    requestURL =  serverFindingService + apiInquery + filterURL;
+    console.log( requestURL );
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", URL, false);
+    xhr.open("GET", requestURL, false);
     try {
         xhr.send();
     } catch (error) {
@@ -23,10 +128,7 @@ app.get('/api/zipauto/:prefix' , (req, res) => {
         return;
     }
     res.send(xhr.responseText);
-});
 
-app.get('/api/courses', (req ,res) => {
-    res.send([1,2,3,4]);
 });
 
 
